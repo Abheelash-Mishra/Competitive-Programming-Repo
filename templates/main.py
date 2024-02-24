7555ee7abc43e80d5e8bfe4f7616e18c8255b00c
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-02-24 17:14:36 +0530

    Initial Commit

---
+++ templates/main.py
@@ -0,0 +1,14 @@
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+
+
+if __name__ == "__main__":
+    
