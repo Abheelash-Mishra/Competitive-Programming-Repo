c250b468f6c39e6c6a072dfddc1a6ce9071a574a
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-02-25 11:05:13 +0530

    Solved Problem 18 (Implementation)

---
+++ Implementation/18_Word Capitalization.py
@@ -0,0 +1,18 @@
+################ ---- Input Functions ---- ################
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+
+############ ---- End Of Input Functions ---- ############
+
+
+if __name__ == "__main__":
+    word = input()
+
+    print(word[0].upper() + word[1:])
